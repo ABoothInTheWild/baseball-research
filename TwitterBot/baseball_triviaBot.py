92c47e93123778cb3a5da26495629495d4e420d6
Alexander Booth <alexanderbooth2019@u.northwestern.edu>
2017-12-14 19:34:36 -0600

    Add files via upload
    Add dynamic pluralization and update tags

--- TwitterBot/baseball_triviaBot.py
+++ TwitterBot/baseball_triviaBot.py
@@ -12,14 +12,16 @@ Created on Fri Nov 24 19:51:08 2017
 from __future__ import division, print_function
 import random
 import pandas as pd
+from twitterBot import *
 
 class BaseballTrivia(TwitterBot):
-    
+
     #inits
-    name = "baseball_trivya"
-    retweetTags = ["#mlb", "#mlbhotstove", "#cfbplayoff"]
-    followTags = []
-    
+    screen_name = "baseball_trivya"
+    retweetTags = ["mlb", "hotstove", "marlins", "machado", "#cfbplayoff","nfl", "nba", "nhl", "ncaa"]
+    favoriteTags = ["mlb", "hotstove","marlins", "machado", "#cfbplayoff", "nfl", "nba", "nhl", "ncaa"]
+    followTags = ["mlb", "hotstove", "marlins", "machado", "#cfbplayoff", "nfl", "nba", "nhl", "ncaa"]
+
     #define data
     master = pd.read_csv(r"https://raw.githubusercontent.com/ABoothInTheWild/baseball-research/master/TwitterBot/Data/Master.csv")
     batting = pd.read_csv(r"https://raw.githubusercontent.com/ABoothInTheWild/baseball-research/master/TwitterBot/Data/Batting.csv")
@@ -30,7 +32,7 @@ class BaseballTrivia(TwitterBot):
     pitching["IP"] = pitching.IPouts / 3.0
     pitching["WHIP"] = (pitching.H + pitching.BB) / pitching.IP
     uniquePlayers = master.playerID.unique()
-    
+
     def getStatus(self):
         path = random.randint(1, 2)
         status = "I hope you have a great day!"
@@ -38,116 +40,107 @@ class BaseballTrivia(TwitterBot):
             status = self.getBattingLine(self.uniquePlayers, self.batting, self.teams, self.master)
         else:
             status = self.getPitchingLine(self.uniquePlayers, self.pitching, self.teams, self.master)
-        
+
         return status
-    
+
     #Get Batting Status
     def getBattingLine(self, uniquePlayers, batting, teams, master):
-    
+
         batData = batting.iloc[[random.randint(0, len(batting)-1)]]
-        
+
         if (not batData.playerID.values[0] in uniquePlayers) or (batData.PA.values[0] == 0):
             self.getBattingLine(uniquePlayers, batting, teams, master)
-            
+
         else:
             name = master[master.playerID == batData.playerID.values[0]].nameFirst.values[0] + \
                     " " + master[master.playerID == batData.playerID.values[0]].nameLast.values[0]
-        
+
             year = batData.yearID.values[0].astype(int)
-            
+
             teamName = teams[(teams.teamID == batData.teamID.values[0]) &
                              (teams.yearID == batData.yearID.values[0])].name.values[0]
-            
+
             games = batData.G.values[0].astype(int)
             plate = batData.PA.values[0].astype(int)
-            
+
             hits = batData.H.values[0].astype(int)
             hrs = batData.HR.values[0].astype(int)
             sos = batData.SO.values[0].astype(int)
-            
+
             line1 = name + " played for the " + teamName + " in the year " + str(year) + ". He had " + \
-                    str(plate) + " plate appearances in " + str(games) + " games with " + str(hits) + " hits, " + \
-                    str(hrs) + " homeruns, and " + str(sos) + " strikeouts. #MLB #facts"
-                    
+                    str(plate) + " plate appearance{s} in ".format(s="s" if plate != 1 else "") + str(games) + \
+                    " game{s} with ".format(s="s" if games != 1 else "") + str(hits) + \
+                    " hit{s}, ".format(s="s" if hits != 1 else "") + str(hrs) + " homerun{s}, and ".format(s="s" if hrs != 1 else "") + \
+                    str(sos) + " strikeout{s}. #MLB #facts".format(s="s" if sos != 1 else "")
+
             line2 = "In the year " + str(year) + ", " + name + " played for the " + teamName +  ". He had " + \
-                    str(plate) + " plate appearances in " + str(games) + " games with " + str(hits) + " hits, " + \
-                    str(hrs) + " homeruns, and " + str(sos) + " strikeouts. #MLB #trivia"
-                    
+                    str(plate) + " plate appearance{s} in ".format(s="s" if plate != 1 else "") + str(games) + \
+                    " game{s} with ".format(s="s" if games != 1 else "") + str(hits) + " hit{s}, ".format(s="s" if hits != 1 else "") + str(hrs) + \
+                    " homerun{s}, and ".format(s="s" if hrs != 1 else "") + str(sos) + " strikeout{s}. #MLB #trivia".format(s="s" if sos != 1 else "")
+
             line3 = name + " played for the " + teamName + " in the year " + str(year) + ". He had " + \
-                    str(hits) + " hits, " + str(hrs) + " homeruns, and " + str(sos) + " strikeouts while recording " + \
-                    str(plate) + " plate appearances in " + str(games) + " games! #MLB #trivia"  
-                    
+                    str(hits) + " hit{s}, ".format(s="s" if hits != 1 else "") + str(hrs) + " homerun{s}, and ".format(s="s" if hrs != 1 else "") + \
+                    str(sos) + " strikeout{s} while recording ".format(s="s" if sos != 1 else "") + str(plate) + \
+                    " plate appearance{s} in ".format(s="s" if plate != 1 else "") + str(games) + " game{s}! #MLB #trivia".format(s="s" if games != 1 else "")
+
             line4 = "In the year " + str(year) + ", " + name + " played for the " + teamName +  ". He had " + \
-                    str(hits) + " hits, " + str(hrs) + " homeruns, and " + str(sos) + " strikeouts while recording " + \
-                    str(plate) + " plate appearances in " + str(games) + " games! #MLB #facts"
-        
+                    str(hits) + " hit{s}, ".format(s="s" if hits != 1 else "") + str(hrs) + " homerun{s}, and ".format(s="s" if hrs != 1 else "") + \
+                    str(sos) + " strikeout{s} while recording ".format(s="s" if sos != 1 else "") + str(plate) + \
+                    " plate appearance{s} in ".format(s="s" if plate != 1 else "") + str(games) + " game{s}! #MLB #facts".format(s="s" if games != 1 else "")
+
             lines = [line1, line2, line3, line4]
-            
+
             finalLine = lines[random.randint(0, 3)]
-            
+
             return finalLine
-    
+
     #Get Pitching Status
     def getPitchingLine(self, uniquePlayers, pitching, teams, master):
-    
+
         pitData = pitching.iloc[[random.randint(0, len(pitching)-1)]]
-        
+
         if (not pitData.playerID.values[0] in uniquePlayers) or (pitData.IP.values[0] == 0):
             self.getPitchingLine(uniquePlayers, pitching, teams, master)
-            
+
         else:
             name = master[master.playerID == pitData.playerID.values[0]].nameFirst.values[0] + \
                     " " + master[master.playerID == pitData.playerID.values[0]].nameLast.values[0]
-        
+
             year = pitData.yearID.values[0].astype(int)
-            
+
             teamName = teams[(teams.teamID == pitData.teamID.values[0]) &
                              (teams.yearID == pitData.yearID.values[0])].name.values[0]
-            
+
             games = pitData.G.values[0].astype(int)
             era = pitData.ERA.values[0].astype(float)
-            
+
             whip = round(pitData.WHIP.values[0].astype(float),2)
             ips = round(pitData.IP.values[0].astype(float), 2)
             wins = pitData.W.values[0].astype(int)
             loss = pitData.L.values[0].astype(int)
-            
+
             line1 = name + " pitched for the " + teamName + " in the year " + str(year) + ". He pitched " + \
-                    str(ips) + " innings over " + str(games) + " games with a " + str(era) + " ERA, " + \
+                    str(ips) + " inning{s} over ".format(s="s" if ips != 1 else "") + \
+                    str(games) + " game{s} with a ".format(s="s" if games != 1 else "") + str(era) + " ERA, " + \
                     str(whip) + " WHIP, and a " + str(wins) + "-" + str(loss) + " record. #MLB #trivia"
-                    
+
             line2 = "In the year " + str(year) + ", " + name + " pitched for the " + teamName +  ". He pitched " + \
-                    str(ips) + " innings over " + str(games) + " games with a " + str(era) + " ERA, " + \
+                    str(ips) + " inning{s} over ".format(s="s" if ips != 1 else "") + str(games) + \
+                    " game{s} with a ".format(s="s" if games != 1 else "") + str(era) + " ERA, " + \
                     str(whip) + " WHIP, and a " + str(wins) + "-" + str(loss) + " record. #MLB #facts"
-                    
+
             line3 = name + " pitched for the " + teamName + " in the year " + str(year) + ". He had a " + \
                     str(era) + " ERA, " + str(whip) + " WHIP, and a " + str(wins) + "-" + str(loss) + " record while pitching " + \
-                    str(ips) + " innings over " + str(games) + " games! #MLB #facts"  
-                    
+                    str(ips) + " inning{s} over ".format(s="s" if ips != 1 else "") + str(games) + \
+                    " game{s}! #MLB #facts".format(s="s" if games != 1 else "")
+
             line4 = "In the year " + str(year) + ", " + name + " pitched for the " + teamName +  ". He had a " + \
                     str(era) + " ERA, " + str(whip) + " WHIP, and a " + str(wins) + "-" + str(loss) + " record while pitching " + \
-                    str(ips) + " innings over " + str(games) + " games. #MLB #trivia"  
-        
+                    str(ips) + " inning{s} over ".format(s="s" if ips != 1 else "") + str(games) + \
+                    " game{s}. #MLB #trivia".format(s="s" if games != 1 else "")
+
             lines = [line1, line2, line3, line4]
-            
+
             finalLine = lines[random.randint(0, 3)]
-            
+
             return finalLine
-        
-###########################################################
-
-fb_url = "https://baseball-trivya.firebaseio.com/tweets.json"
-
-#enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '2gBiPBoAovxg7PDNeX6jXTKLX'
-CONSUMER_SECRET = '3YT9LXW7StJ7OoDMHgw8kFVzXZEQK2H2ilnFrseXsUCDLcXSaJ'
-ACCESS_KEY = '931373265653784576-4fgDnw0hbNJ1BAV3egNrh07URyPB6FU'
-ACCESS_SECRET = 'oM1EGxPDqtWiY8Yrvd7j72dMownO7YGYcaqDDhQFREQzh'
-
-twitterBot = BaseballTrivia(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET, fb_url)
-
-while True:
-    twitterBot.doAction()
-
-
-
--- TwitterBot/twitterBot.py
+++ TwitterBot/twitterBot.py
@@ -10,26 +10,29 @@ Created on Fri Nov 24 18:21:39 2017
 # Imports
 # prepare for Python version 3x features and functions
 from __future__ import division, print_function
-import time
+from time import sleep
 import tweepy
 import sys
 import requests
 import json
 import random
 from abc import ABCMeta, abstractmethod
-from datetime import datetime
+import datetime
 
 class TwitterBot(object):
     
     __metaclass__ = ABCMeta
     
     #Get time. Don't post in the middle of the night
-    timeStart = datetime.strptime('7:00AM', "%I:%M%p").time()
-    timeEnd = datetime.strptime('11:00PM', "%I:%M%p").time()
+    timeStart = datetime.datetime.strptime('7:00AM', "%I:%M%p").time()
+    timeEnd = datetime.datetime.strptime('11:00PM', "%I:%M%p").time()
+    timeMOTS = datetime.datetime.strptime('2:00AM', "%I:%M%p").time()
+    timeMOTE= datetime.datetime.strptime('3:00AM', "%I:%M%p").time()
     
     #inits
-    name = ""
+    screen_name = ""
     retweetTags = []
+    favoriteTags = []
     followTags = []
     
     def __init__(self, ckey, csec, akey, asec, fb_url):
@@ -38,40 +41,182 @@ class TwitterBot(object):
         auth = tweepy.OAuthHandler(ckey, csec)
         auth.set_access_token(akey, asec)
         self.api = tweepy.API(auth)
-        
-    def postRetweet(self, tag, minRetweetCount=100):
+    
+    def addFollower(self, tag, minRetweetCount=5, minFavCount = 10):
+        rtnBool = True
         try:
-            c = tweepy.Cursor(self.api.search, q=tag)
-            for tweet in c.items():
-                timeNow = datetime.now().time()
-                if self.isNowInTimePeriod(self.timeStart, self.timeEnd, timeNow) and tweet.retweet_count > minRetweetCount:
-                    if self.check_tweet(tweet):
+            retweetDB = "tweets.json"
+            favDB = "favTweets.json"
+            check = False
+            c = tweepy.Cursor(self.api.search, q=tag).items(500)
+            for tweet in c:
+                if (tweet.retweet_count > minRetweetCount) and (tweet.favorite_count > minFavCount):
+                    if not tweet.user.following:
+                        tweet.user.follow()
+                        print('Followed the user: @' + tweet.user.screen_name)
+                        check = True
+                        #Also favorite it half the time    
+                        if (random.random() >= .5) and (self.checkTweetNotInDB(tweet, favDB)):
+                            tweet.favorite()
+                            db_tweet = {"id": tweet.id}
+                            requests.post(self.fb_url + favDB, json.dumps(db_tweet))
+                            print('Favorited Tweet Id: ' + str(tweet.id))
+                        #Also retweet it occaisionally 
+                        if (random.random() >= .8) and (self.checkTweetNotInDB(tweet, retweetDB)):
+                            self.api.retweet(tweet.id)
+                            db_tweet = {"id": tweet.id}
+                            requests.post(self.fb_url + retweetDB, json.dumps(db_tweet))
+                            print('Retweeted Tweet Id: ' + str(tweet.id))
+                        break
+                else:
+                    continue
+                    
+            if check == False:
+                print("Failed to Follow Anyone")
+                rtnBool = False
+
+            # Exceptions
+        except KeyboardInterrupt:
+            sys.exit("KeyboardInterrupt")
+        except Exception as e:
+            print(e)
+            rtnBool = False
+        return rtnBool
+        
+    def postRetweet(self, tag, minRetweetCount=20):
+        rtnBool = True
+        try:
+            retweetDB = "tweets.json"
+            favDB = "favTweets.json"
+            
+            c = tweepy.Cursor(self.api.search, q=tag).items(100)
+            for tweet in c:
+                if tweet.retweet_count > minRetweetCount:
+                    if self.checkTweetNotInDB(tweet, retweetDB):
                         self.api.retweet(tweet.id)
                         db_tweet = {"id": tweet.id}
-                        requests.post(self.fb_url, json.dumps(db_tweet))
+                        requests.post(self.fb_url + retweetDB, json.dumps(db_tweet))
+                        print('Retweeted Tweet Id: ' + str(tweet.id))
+                        #Also favorite it half the time    
+                        if (random.random() >= .5) and (self.checkTweetNotInDB(tweet, favDB)):
+                            tweet.favorite()
+                            db_tweet = {"id": tweet.id}
+                            requests.post(self.fb_url + favDB, json.dumps(db_tweet))
+                            print('Favorited Tweet Id: ' + str(tweet.id))                    
+                        #Also follow sometimes 
+                        if (random.random() <= .2) and (not tweet.user.following):
+                            tweet.user.follow()
+                            print('Followed the user: @' + tweet.user.screen_name)
                         break
         # Exceptions
         except KeyboardInterrupt:
             sys.exit("KeyboardInterrupt")
         except Exception as e:
             print(e)
-            pass    
-
-    def check_tweet(self, tweet):
+            rtnBool = False
+        return rtnBool
+    
+    def addFavorite(self, tag, minFavoriteCount=20):
+        rtnBool = True
         try:
-            r = requests.get(self.fb_url)
+            retweetDB = "tweets.json"
+            favDB = "favTweets.json"
+            check = False
+            c = tweepy.Cursor(self.api.search, q=tag).items(500)
+            for tweet in c:
+                if tweet.favorite_count > minFavoriteCount:
+                    if self.checkTweetNotInDB(tweet, favDB):
+                        tweet.favorite()
+                        db_tweet = {"id": tweet.id}
+                        requests.post(self.fb_url + favDB, json.dumps(db_tweet))
+                        print('Favorited Tweet Id: ' + str(tweet.id))
+                        check = True
+                        #Also retweet it occaisionally 
+                        if (random.random() >= .75) and (self.checkTweetNotInDB(tweet, retweetDB)):
+                            self.api.retweet(tweet.id)
+                            db_tweet = {"id": tweet.id}
+                            requests.post(self.fb_url + retweetDB, json.dumps(db_tweet))
+                            print('Retweeted Tweet Id: ' + str(tweet.id))
+                        #Also follow sometimes 
+                        if (random.random() <= .2) and (not tweet.user.following):
+                            tweet.user.follow()
+                            print('Followed the user: @' + tweet.user.screen_name)
+                        break
+                else:
+                    continue
+                
+            if check == False:
+                print("Failed to Favorite Anything")
+                rtnBool = False
+
+        # Exceptions
+        except KeyboardInterrupt:
+            sys.exit("KeyboardInterrupt")
+        except Exception as e:
+            print(e)
+            rtnBool = False
+        return rtnBool
+    
+    def followFollowers(self):
+        try:
+            for user in self.api.followers():
+                if not user.following:
+                    user.follow()
+                    print('Followed the user: @' + user.screen_name)
+        # Exceptions
+        except KeyboardInterrupt:
+            sys.exit("KeyboardInterrupt")
+        except Exception as e:
+            print(e)
+            pass  
+    
+    def updateRetweetDatabase(self):
+        try:
+            retweetDB = "tweets.json"
+            tweets = self.api.user_timeline(screen_name = self.screen_name, count = 100, include_rts = True)
+            for tweet in tweets:
+                if tweet._json["retweeted"]:
+                    if self.checkTweetNotInDB(tweet, retweetDB):
+                        db_tweet = {"id": tweet.id}
+                        requests.post(self.fb_url + retweetDB, json.dumps(db_tweet))
+                # Exceptions
+        except KeyboardInterrupt:
+            sys.exit("KeyboardInterrupt")
+        except Exception as e:
+            print(e)
+            pass 
+        
+    def updateFavoriteDatabase(self):
+        try:
+            favDB = "favTweets.json"
+            tweets = tweepy.Cursor(self.api.favorites,id=self.screen_name).items(100)
+            for tweet in tweets:
+                if self.checkTweetNotInDB(tweet, favDB):
+                    db_tweet = {"id": tweet.id}
+                    requests.post(self.fb_url + favDB, json.dumps(db_tweet))
+                # Exceptions
+        except KeyboardInterrupt:
+            sys.exit("KeyboardInterrupt")
+        except Exception as e:
+            print(e)
+            pass  
+        
+    def checkTweetNotInDB(self, tweet, dataBase):
+        rtnBool = True
+        try:
+            r = requests.get(self.fb_url + dataBase)
             r = r.json()
             for i in r:
                 if tweet.id == r[i]["id"]:
-                    return False
+                    rtnBool = False
+                    break
        # Exceptions 
         except KeyboardInterrupt:
             sys.exit("KeyboardInterrupt")
         except Exception as e:
             print(e)
             pass
-        finally:
-            return True  
+        return rtnBool
     
     @staticmethod
     def isNowInTimePeriod(startTime, endTime, nowTime):
@@ -87,13 +232,17 @@ class TwitterBot(object):
     def sleepyBaby():
         time1 = random.randint(1, 4)
         if time1 == 1:
-            time.sleep(2100) #Tweet every 35 minutes
+            print("Sleeping for 35min...")
+            sleep(2100) #Tweet every 35 minutes
         elif time1 == 2:
-            time.sleep(3900) #Tweet every 65 minutes
+            print("Sleeping for 65min...")
+            sleep(3900) #Tweet every 65 minutes
         elif time1 == 3:
-            time.sleep(2760) #Tweet every 46 minutes
+            print("Sleeping for 46min...")
+            sleep(2760) #Tweet every 46 minutes
         else:
-            time.sleep(1410) #Tweet every 23.5 minutes
+            print("Sleeping for 23.min...")
+            sleep(1410) #Tweet every 23.5 minutes
      
     @abstractmethod
     def getStatus(self):
@@ -102,28 +251,73 @@ class TwitterBot(object):
     
     def postStatus(self, status):
         rtnBool = True
-        timeNow = datetime.now().time()
-        if self.isNowInTimePeriod(self.timeStart, self.timeEnd, timeNow):
-            if status and not status is None:
-                try:
-                    self.api.update_status(status)
-                # Additions below
-                except KeyboardInterrupt:
-                    sys.exit("KeyboardInterrupt")
-                except Exception as e:
-                    print(e)
-                    rtnBool = False
-        return rtnBool
-    
-    def doAction(self):
-        potentialActions = ["retweet", "status", "status", "nothing", "nothing"]
-        action = random.choice(potentialActions)
-        
-        if action == "retweet":
-            self.postRetweet(random.choice(self.retweetTags))
-        elif action == "status":
-            self.postStatus(self.getStatus())
+        if status and not status is None:
+            try:
+                self.api.update_status(status)
+                print('Created a Status: ' + status[0:50] + "...")
+            # Additions below
+            except KeyboardInterrupt:
+                sys.exit("KeyboardInterrupt")
+            except Exception as e:
+                print(e)
         else:
-            pass
+            print("Failed to create a status this time")
+            rtnBool = False
+        return rtnBool
+
+    def doAction(self):
+        print("Trying to do an Action...")
+        potentialActions = ["retweet", "follow", "favorite", "favorite", "status", "status", "status", "nothing"]
+        action = random.choice(potentialActions)
+        statBool = True
+        #For Central from UTC time zone in pythonAnywhere
+        timeNow = (datetime.datetime.now() - datetime.timedelta(hours=6)).time()
+        if self.isNowInTimePeriod(self.timeStart, self.timeEnd, timeNow):
+            if action == "retweet":
+                print("Chose Retweet")
+                statBool = self.postRetweet(random.choice(self.retweetTags))
+            elif action == "status":
+                print("Chose Status")
+                statBool = self.postStatus(self.getStatus())
+            elif action == "favorite":
+                print("Chose Favorite")
+                rInt = random.randint(1, 5)
+                for x in range(0, rInt):
+                    statBool = self.addFavorite(random.choice(self.favoriteTags))
+                    if (statBool == False) and (x > 0):
+                        statBool = True
+                        break                        
+                    if statBool == False:
+                        break
+            elif action == "follow":
+                print("Chose Follow")
+                rInt = random.randint(1, 3)
+                for x in range(0, rInt):
+                    statBool = self.addFollower(random.choice(self.followTags))
+                    if (statBool == False) and (x > 0):
+                        statBool = True
+                        break 
+                    if statBool == False:
+                        break
+            else:
+                print("Chose to do Nothing this time... *shrug*")
+                statBool = True
+                pass
+        else:
+            print("Outside time to do any actions... " + str(timeNow))
         
-        self.sleepyBaby()
+        #Check for manual retweets and favorites not logged in db
+        #Also follow any new followers
+        if self.isNowInTimePeriod(self.timeMOTS, self.timeMOTE, timeNow):
+            print("Middle of the night... " + str(timeNow))
+            print("Updating Databases")
+            self.updateRetweetDatabase()   
+            self.updateFavoriteDatabase()
+            print("Following Followers")
+            self.followFollowers()
+        
+        if statBool:
+            self.sleepyBaby()
+        else:
+            print("Failed to complete an action. Trying again in 2.5min")
+            sleep(250)
